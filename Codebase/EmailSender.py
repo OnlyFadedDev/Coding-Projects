cf0cc6af559c3ea96ad686f73d542f4adf66cb6e
Anthony <ruizanthony1243@gmail.com>
2024-09-02 14:23:42 -0500

    dad

--- Codebase/EmailSender.py
+++ Codebase/EmailSender.py
@@ -8,15 +8,15 @@ from pathlib import Path
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Andrei Neagoie'
-email['to'] = 'testingemail@'
+email['to'] = 'andrei@zerotomastery.to'
 email['subject'] = 'You won 1,000,000 dollars!'
 
-email.set_content(html.substitute({'name': 'TinTin'}), 'html')
+email.set_content(html.substitute({'name': 'TinTin'}))
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
   smtp.ehlo()
   smtp.starttls()
-  smtp.login('<your email address>', '<your password>')
+  smtp.login('testingemail4412@gmail.com', 'Basepassword')
   smtp.send_message(email)
   print('all good boss!')
 
