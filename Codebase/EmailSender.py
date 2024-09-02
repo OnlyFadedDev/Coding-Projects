b619fc253fab25e1b0ccf8fca1fcf3ae2ce02e46
Anthony <ruizanthony1243@gmail.com>
2024-09-02 14:33:32 -0500

    new version

--- Codebase/EmailSender.py
+++ Codebase/EmailSender.py
@@ -1,22 +1,22 @@
-# Sending an email with Python
+# Sending an email with Python also reading an html file and sending it out as normal text.
 
 import smtplib
 from email.message import EmailMessage
 from string import Template
 from pathlib import Path 
 
-html = Template(Path('index.html').read_text())
+html = Template(Path('C:\VScode\Codebase\index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Andrei Neagoie'
-email['to'] = 'andrei@zerotomastery.to'
+email['to'] = 'andrei@zerotomastery.io'
 email['subject'] = 'You won 1,000,000 dollars!'
 
-email.set_content(html.substitute({'name': 'TinTin'}))
+email.set_content(html.substitute(name= 'TinTin'), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
   smtp.ehlo()
   smtp.starttls()
-  smtp.login('testingemail4412@gmail.com', 'Basepassword')
+  smtp.login('testingemail4412@gmail.com', 'fsuv ahul rose prlr') # SMTPAuthenticationError: Need an app password from your google account.
   smtp.send_message(email)
   print('all good boss!')
 
