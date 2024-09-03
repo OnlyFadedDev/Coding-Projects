25553108a23f01a365ab15272966438c4a8c3e4b
Anthony <ruizanthony1243@gmail.com>
2024-09-02 20:57:57 -0500

    Fixed

--- Codebase/SmsSender.py
+++ Codebase/SmsSender.py
@@ -1,14 +1,14 @@
 # SMS With Python
 
 from twilio.rest import Client
-
-account_sid = 'AC1e7098980c53b296e851478742bbd1d8'
-auth_token = '3e6ce64f503ca4aeb7b02da1e7312d82'
+ 
+account_sid = '' # Secret
+auth_token = '' # Secret
 client = Client(account_sid, auth_token)
 message = client.messages.create(
-  messaging_service_sid='MG2f45a2737d1e4d1fa696b4558fb4ab22',
+  messaging_service_sid='', # Secret
   body='Ahoy 👋',
-  to=''
+  to='' # Secret
 )
 print(message.sid)
 
