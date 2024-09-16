7d8df339695665d1e67dee16a2354d1b63e573d1
Anthony <ruizanthony1243@gmail.com>
2024-09-15 19:17:41 -0500

    asd

--- Codebase/webserver/server.py
+++ Codebase/webserver/server.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
+# python -m flask run
+# ^ to run server
 
 app = Flask(__name__)
-print(__name__) 
+print(__name__) # __main__
+ 
+@app.route("/<username>/<int:post_id>") 
+def hello_world(username=None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id )
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/about.html") 
+def about():
+    return render_template('about.html')
+
+@app.route("/favicon.ico") 
+def blog():
+    return "These are my thoughts on blogs"
+
+@app.route("/blog/2020/dogs") 
+def blog2():
+    return "This is my dog"
